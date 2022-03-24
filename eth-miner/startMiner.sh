5e8890b5be87ffe2e6c6e4b05224c49dd4894a2c
bangdracaena <bang@dracaena.io>
2022-03-24 02:47:28 -0700

    update script and md5sum code

--- eth-miner/startMiner.sh
+++ eth-miner/startMiner.sh
@@ -19,9 +19,7 @@ ipaddress=192.168.1.159
 #phase = 1: use best frequencies from text file. If there is no text file, start autotuning
  
 #phase = 4: use frequencies from command line
-phase=4
-
-sudo  ./ethminer --fpga -a 530 -b 312 -c 125 -q 100 -e 4 -f -d 70  -P stratum1+tcp://0x2784685ba4a940406b185f945c26104d64f7562e.vu35p@eth-na.f2pool.com:6688  --ip 192.168.1.62
+phase=1
 
 while true; do
 			sudo killall ethminer
